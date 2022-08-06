602472cb6764d2b685f5410e3abd1329584923dc
Your Name <you@example.com>
2022-08-06 11:37:15 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,119 @@
+const theBox = document.getElementById("mybox")
+const myText = document.getElementById("mytextname")
+
+let but1 = documnet.getElementById("btn1")
+let but2 = documnet.getElementById("btn2")
+let but3 = documnet.getElementById("btn3")
+let but4 = documnet.getElementById("btn4")
+let but5 = documnet.getElementById("btn5")
+let but6 = documnet.getElementById("btn6")
+let but7 = documnet.getElementById("btn7")
+let but8 = documnet.getElementById("btn8")
+let but9 = documnet.getElementById("btn9")
+let but0 = documnet.getElementById("btn0")
+let but01 = documnet.getElementById("btn01")
+let but02 = documnet.getElementById("btn02")
+let but03 = documnet.getElementById("btn03")
+let but04 = documnet.getElementById("btn04")
+let but05 = documnet.getElementById("btn05")
+let but06 = documnet.getElementById("btn06")
+let but07 = documnet.getElementById("btn07")
+let but08 = documnet.getElementById("btn08")
+let but10 = documnet.getElementById("btn10")
+
+function div1(){
+    theBox.style.backgroundColor = "white"
+    myText.innerHTML = "White"
+    theBox.style.color = "black"
+}
+function div2(){
+    theBox.style.backgroundColor = "black"
+    myText.innerHTML = "Black"
+    colorText()  
+}
+function div3(){
+    theBox.style.backgroundColor = "red"
+    myText.innerHTML = "Red"
+    colorText()
+}
+function div4(){
+    theBox.style.backgroundColor = "green"
+    myText.innerHTML = "Green"
+    colorText()
+}
+function div5(){
+    theBox.style.backgroundColor ="blue"
+    myText.innerHTML = "Blue"
+}
+function div6(){
+    theBox.style.backgroundColor = "yellow"
+    myText.innerHTML = "Yellow"
+    colorText()
+}
+function div7(){
+    theBox.style.backgroundColor = "grey"
+    myText.innerHTML = "Grey"
+    colorText()
+}
+function div8(){
+    theBox.style.backgroundColor = "orange"
+    myText.innerHTML = "Orange"
+    colorText()
+}
+function div9(){
+    theBox.style.backgroundColor = "purple"
+    myText.innerHTML = "Purple"
+    colorText()
+}
+function div0(){
+    theBox.style.backgroundColor = "brown"
+    myText.innerHTML = "Brown"
+    colorText()
+}
+function div01(){
+    theBox.style.backgroundColor = "olive"
+    myText.innerHTML = "Olive"
+    colorText()
+}
+function div02(){
+    theBox.style.backgroundColor = "maroon"
+    myText.innerHTML = "Maroon"
+    colorText()
+}
+function div03(){
+    theBox.style.backgroundColor = "violet"
+    myText.innerHTML = "Voilet"
+    colorText()
+}
+function div04(){
+    theBox.style.backgroundColor = "chocolate"
+    myText.innerHTML = "Chocolate"
+    colorText()
+}
+function div05(){
+    theBox.style.backgroundColor ="teal"
+    myText.innerHTML = "Teal"
+    colorText()
+}
+function div06(){
+    theBox.style.backgroundColor = "cyan"
+    myText.innerHTML = "Cyan"
+}
+function div07(){
+    theBox.style.backgroundColor = "gold"
+    myText.innerHTML = "Gold"
+    colorText()
+}
+function div08(){
+    theBox.style.backgroundColor = "tan"
+    myText.innerHTML = "Tan"
+    colorText()
+}
+function div10(){
+    theBox.style.backgroundColor = "pink"
+    myText.innerHTML = "Pink"
+    colorText()
+}
+function colorText(){
+    theBox.style.color = "white"
+}
